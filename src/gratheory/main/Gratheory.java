e3d0254794c76b865ef777bddabf3b7ad79befbd
molinab297 <molinab297@gmail.com>
2017-03-29 15:25:07 -0700

    - Bug fixes

--- src/gratheory/main/Gratheory.java
+++ src/gratheory/main/Gratheory.java
@@ -2,20 +2,20 @@ package gratheory.main;
 import gratheory.algorithms.Algorithms;
 import processing.core.PApplet;
 
-// Main driver class
 public class Gratheory extends PApplet{
 
     public static void main(String[] args) { PApplet.main("gratheory.main.Gratheory"); }
+
+    // Color constants
+    public static final int WHITE = 255, GRAY = 167, BLACK = 0;
+
     public static final int SCREEN_WIDTH   = 1000;
     public static final int SCREEN_HEIGHT  = 650;
-    public static final int GRAY           = 167;
-    public static final int WHITE          = 255;
-    public static final int BLACK          = 0;
     private final String INPUT_FILE        = "/Users/Blake/IdeaProjects/Gratheory/src/gratheory/resources/AdjancyMatrix.txt";
     private Graph graph;
     private ButtonList vertexOptions, graphOptions;
     private boolean vertexClicked, addEdgeSelected, graphOptionWindow;
-    private int id = 0;
+    private int vertexID = 0;
     private int startVertex, endVertex;
 
     public void settings(){
@@ -45,18 +45,6 @@ public class Gratheory extends PApplet{
         update();
     }
 
-
-    public void update(){
-        if(vertexClicked){
-            vertexOptions.setCoordinates(graph.getVertex(id).getxCoord() + 10, graph.getVertex(id).getyCoord() + 10);
-            vertexOptions.display();
-            graphOptionWindow = false;
-        }
-        else if(graphOptionWindow){
-            graphOptions.display();
-        }
-    }
-
     public void mouseClicked() {
 
         // Left click on vertices to select vertex options or graph options
@@ -76,6 +64,27 @@ public class Gratheory extends PApplet{
         }
     }
 
+    // Checks if vertex is being dragged. If it is, make vertex follow user's mouse.
+    public void mouseDragged(){
+        for (Vertex v : graph.getVertexList())
+            if (dist(v.getxCoord(), v.getyCoord(), mouseX, mouseY) < 40) {
+                if (mousePressed)
+                    v.setCoordinates(mouseX, mouseY);
+            }
+    }
+
+    public void update(){
+        if(vertexClicked){
+            vertexOptions.setCoordinates(graph.getVertex(vertexID).getxCoord()
+                    + 10, graph.getVertex(vertexID).getyCoord() + 10);
+            vertexOptions.display();
+            graphOptionWindow = false;
+        }
+        else if(graphOptionWindow){
+            graphOptions.display();
+        }
+    }
+
     // Checks if user is clicking a vertex
     private void checkIfClickingVertex(){
         for (Vertex v : graph.getVertexList()) {
@@ -83,7 +92,7 @@ public class Gratheory extends PApplet{
                 if (vertexClicked)
                     vertexClicked = false;
                 else {
-                    id = v.getId();
+                    vertexID = v.getId();
                     vertexClicked = true;
                 }
             }
@@ -93,22 +102,21 @@ public class Gratheory extends PApplet{
         if (vertexClicked) {
             if (vertexOptions.getButton(0).overButton(mouseX, mouseY)) {
                 graph.clear();
-                Algorithms.DFS(graph, graph.getVertex(id));
+                Algorithms.DFS(graph, graph.getVertex(vertexID));
             }
             else if (vertexOptions.getButton(1).overButton(mouseX, mouseY)) {
                 graph.clear();
-                Algorithms.BFS(graph, graph.getVertex(id));
+                Algorithms.BFS(graph, graph.getVertex(vertexID));
             }
             else if (vertexOptions.getButton(2).overButton(mouseX, mouseY)) {
-                startVertex = id;
+                startVertex = vertexID;
                 addEdgeSelected = true;
                 vertexClicked = false;
             }
             else if(addEdgeSelected){
-                endVertex = id;
+                endVertex = vertexID;
                 graph.addEdge(graph.getVertex(startVertex), graph.getVertex(endVertex),WHITE,0);
                 addEdgeSelected = false;
-                System.out.println(graph.matrixToString());
             }
         }
     }
@@ -117,20 +125,13 @@ public class Gratheory extends PApplet{
     private void checkIfClickingGraphOptions(){
         if(graphOptions.getButton(0).overButton(mouseX,mouseY)){
             graph.addVertex(new Vertex(mouseX, mouseY, graph.VERTEX_SIZE, graph.VERTEX_SIZE, graph.VERTEX_COLOR, graph.getVertexCount(), this));
-            System.out.println(graph.matrixToString());
+            graphOptionWindow = false;
         }
         else if(graphOptions.getButton(1).overButton(mouseX,mouseY)){
             graph.saveGraph(INPUT_FILE);
+            graphOptionWindow = false;
         }
     }
 
-    // Checks if vertex is being dragged. If it is, make vertex follow user's mouse.
-    public void mouseDragged(){
-        for (Vertex v : graph.getVertexList())
-            if (dist(v.getxCoord(), v.getyCoord(), mouseX, mouseY) < 40) {
-                if (mousePressed)
-                    v.setCoordinates(mouseX, mouseY);
-            }
-    }
 
 }
